5323cf8a28e4e51f4523500c5c7a9e3728aaf923
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-04-18 04:24:57 +0200

    style(ui): :lipstick: update Badge design for better visibility

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 export const Badge = ({ items }: { items: string[] }) => {
   return (
-    <ul className='flex flex-wrap gap-2 my-4'>
+    <ul className='flex flex-wrap items-center justify-center gap-2 my-4'>
       {items.map((item) => (
         <li
           key={item}
-          className='bg-[#12042d]/40 text-[#b2b2ff] text-sm font-medium px-2.5 py-1 rounded-xl border border-[#7042f8]'
+          className='bg-[#3f007d]/40 text-[#b2b2ff] text-sm font-medium px-2.5 py-1 rounded-xl border border-[#b2b2ff]'
         >
           {item}
         </li>
