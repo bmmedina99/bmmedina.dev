821c512d1c2ecdeb4d93bd339f1393a163f9f936
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-08-26 13:08:26 +0200

    chore(config): :wrench: add `site.config.ts`

---
+++ src/site.config.ts
@@ -0,0 +1,8 @@
+export const configSite = {
+  title: 'bmmedina',
+  description:
+    'Brandon Medina, desarrollador web Full Stack con más de 3 años de experiencia en Next.js, Astro, React y Node.js. Enfocado en crear y optimizar sitios web.',
+  lang: 'es',
+  author: 'bmmedina99',
+  url: 'https://bmmedina.dev',
+}
